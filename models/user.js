83128f03cb9d35297e9a9733a48c0c52a5722971
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-12-04 14:24:12 -0800

    salt password with bcrypt

--- models/user.js
+++ models/user.js
@@ -1,10 +1,31 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 // Define model
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String
+});
+
+// On Save Hook, encrypt password
+// Before saving a model, run this function
+userSchema.pre('save', function(next) {
+  // Get access to the user model
+  const user = this; // user.email, user.password
+
+  // Generate a salt then run callback
+  bcrypt.genSalt(10, function(error, salt) {
+    if (error) { return next(error); }
+
+    // Hash password using the salt
+    bcrypt.hash(user.password, salt, null, function(error, hash) {
+      if (error) { return next(error); }
+
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 // Create model class
